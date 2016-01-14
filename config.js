f0e31ccc1e16cf8d5614933c13357cc530710d21
bahadur <bahadurfarooq89@gmail.com>
2016-01-14 21:29:11 +0500

    adding all the files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports ={
+    "database":"mongodb://root:abc123@ds037195.mongolab.com:37195/userstoryapp",
+    "port":process.env.PORT || 3000,
+    "secretKey": "YourSecretKey"
+}
---
+++ public/app/app.js
@@ -0,0 +1,34 @@
+angular.module('MyApp', ['appRoutes','mainCtrl','authService','userCtrl','userService','storyService'])
+.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+})
+    .controller('StoryController', function (Story, socketio) {
+        var vm = this;
+        Story.all()
+            .success(function (data) {
+                vm.stories = data;
+            });
+        vm.createStory = function () {
+            vm.message = '';
+            Story.create(vm.storyData)
+                .success(function (data) {
+                    vm.processing = false;
+                    //clear up the form
+                    vm.storyData = '';
+                    vm.message = data.message;
+                });
+        };
+        socketio.on('story', function (data) {
+            vm.stories.push(data)
+        })
+    })
+    .controller('AllStoriesController', function(stories, socketio){
+        var vm = this;
+        vm.stories = stories.data;
+        socketio.on('story', function (data) {
+            vm.stories.push(data);
+        })
+    });
+
+
+//'
